ff7c1e2466263408b2665be078632adf340d3ce9
Smirnov Nikita <zigman.nikita@gmail.com>
2021-08-27 14:06:53 +0200

    cleaned a bit

--- robot_driver_class.py
+++ robot_driver_class.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
+import board
 import pydantic
 import digitalio
 from Adafruit_MCP4725 import MCP4725
        
 
-class Driver:
+class Driver(pydantic.BaseModel):
     """the class incapsulate interface of the driver
 
         it uses 2 GPIO keys to control switches of propulsion and steering drivers and 
@@ -16,70 +17,41 @@ class Driver:
         forward = 1
         backward = 2
         stop = 3
+    
+    MAX_VALUE = 4095
 
-    DELAY_COEFICIENT = 10
-
-    def __init__(self, motor_dict: dict, name: str) -> None:
-        """it's initializes the driver class
-
-        Args:
-            motor_dict (dict): part of setting dict related to the driver (propulsion or steering)
-        """
-        self.name = name
+    forward_pin: int
+    backward_pin: int
+    i2c_adress: int = 0x60
+    
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
 
         self.current_direction = self.Direction.stop
 
-        self.dac = MCP4725(address=motor_dict['I2C']['address'])
-        self.dac.set_voltage(4095)
+        self.dac = MCP4725(address=self.i2c_adress)
+        self.dac.set_voltage(self.MAX_VALUE)
 
-        self.forward_gpio = digitalio.DigitalInOut(motor_dict['forward']['pin'])
-        self.forward_gpio.direction = motor_dict['forward']['direction']
+        self.forward_gpio = digitalio.DigitalInOut(board.__dict__[f'D{self.forward_pin}'])
+        self.forward_gpio.direction = digitalio.Direction.OUTPUT
         self.forward_gpio.value = False
 
-        self.backward_gpio = digitalio.DigitalInOut(motor_dict['backward']['pin'])
-        self.backward_gpio.direction = motor_dict['backward']['direction']
+        self.backward_gpio = digitalio.DigitalInOut(board.__dict__[f'D{self.backward_pin}'])
+        self.backward_gpio.direction = digitalio.Direction.OUTPUT
         self.backward_gpio.value = False
     
     def __del__(self):
         self.stop_motor()
         self.dac.set_voltage(4096, persist=True)
-
-    def _check_dict(self, motor_dict: dict) -> None:
-        """it checks the structure of the dict
-
-        Args:
-            motor_dict (dict): part of setting dict related to the driver (propulsion or steering)
-
-        Returns:
-            bool: True - all is fine, False - structure of @motor_dict is wrong
-        """
-
-        if not {'forward','backward','I2C'} <= set(motor_dict.keys()):
-            raise Exception("motor_settig should have 'forward' and 'backward' keys")
-        
-        for key,value in motor_dict.items():
-            
-            if not {'pin','direction'} <= set(value.keys()):
-                raise Exception(f"{key} of motor_settig should have 'pin' and 'direction' keys")
-            elif not 'address' in value.keys():
-                raise Exception(f"{key} of motor_settig should have 'address' key")
-        
-        return True
-    
+   
     def move(self, value: int) -> None: 
-        """it makes robot move
+       
+        if not -100 <= value <= 100:
+            raise ValueError("value should be in range of [0:127]")
 
-        Args: 
-            value (int): the value in [0:127] of moter power
-        """
-        
-        if not -127 <= value <= 127:
-            raise Exception("value should be in range of [0:127]")
+        inverted_value = 100 - abs(value)
 
-        normalised_value = (value / 127.0)*100
-        normalised_value_inverted = 100 - abs(normalised_value)
-
-        if normalised_value > 0:
+        if value > 0:
             
             if self.current_direction == self.Direction.backward:
                 self.stop_motor()
@@ -87,7 +59,7 @@ class Driver:
             self.current_direction = self.Direction.forward
             self.forward_gpio.value = True
 
-        elif normalised_value < 0:
+        elif value < 0:
 
             if self.current_direction == self.Direction.forward:
                 self.stop_motor()
@@ -95,10 +67,10 @@ class Driver:
             self.current_direction = self.Direction.backward
             self.backward_gpio.value = True
 
-        elif normalised_value == 0:
+        elif value == 0:
             self.stop_motor()
             
-        self.dac.set_voltage(int((4096 * normalised_value_inverted) / 100))
+        self.dac.set_voltage(int((4096 * inverted_value) / 100))
       
     def stop_motor(self) -> None:
         if self.current_direction != self.Direction.stop:
